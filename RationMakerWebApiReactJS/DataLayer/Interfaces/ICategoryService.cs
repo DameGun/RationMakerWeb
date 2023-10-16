493cec6cdc5d7aff8d92e6285a477f1b2e6bec8b
Ilya Prymak <53339033+DameGun@users.noreply.github.com>
2023-10-16 12:49:09 +0300

    Delete RationMakerWebApiReactJS directory

--- RationMakerWebApiReactJS/DataLayer/Interfaces/ICategoryService.cs
+++
@@ -1,8 +0,0 @@
-using RationMakerWebApiReactJS.DataLayer.Models;
-
-namespace RationMakerWebApiReactJS.DataLayer.Interfaces;
-
-public interface ICategoryService
-{
-    Task<IEnumerable<Category>?> GetAllAsync();
-}
